ac75e3eedeebb2467f93a2592be1d93a5e7c208b
M. Devi Sri Sravani <96713237+M-DeviSri@users.noreply.github.com>
2022-02-23 20:37:26 +0530

    Add files via upload
    largest of two numbers...

---
+++ largest.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+if a>b:
+    print(a," is Big")
+else:
+    print(a," is Small")
+
+
+
+OUTPUT:
+
+C:\python>largest.py
+Enter a value: 2
+Enter b value: 1
+2  is Big
