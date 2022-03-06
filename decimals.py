d10b36e478663b5ae279a4a544ee5fd5c2b784a3
M. Devi Sri Sravani <96713237+M-DeviSri@users.noreply.github.com>
2022-03-06 19:38:00 +0530

    Update decimals.py

--- decimals.py
+++ decimals.py
@@ -2,3 +2,13 @@ print("This program is to print the decimal values of 1/1,1/2,1/3,.....1/n")
 n = int(input("Enter the number of terms to be print:"))
 for i in range(1,n+1):
     print(1/i)
+
+    
+#OUTPUT
+This program is to print the decimal values of 1/1,1/2,1/3,.....1/n
+Enter the number of terms to be print:5
+1.0
+0.5
+0.3333333333333333
+0.25
+0.2
